944cea6b3fb6d74d9fd99ee6bfa172d4a228b542
Aman Verma <132205172+Aman10Verma@users.noreply.github.com>
2024-12-13 16:43:39 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+# Take input from the user
+user_input = input("Enter a string:") # Prompt the user to enter a string
+# Capitalise the first letter of each word
+# The title() method capitalizes the first letter of each word
+capitalized_string = user_input.title()
+# Print the result
+print("Capitalized string is: ", capitalized_string ) 
